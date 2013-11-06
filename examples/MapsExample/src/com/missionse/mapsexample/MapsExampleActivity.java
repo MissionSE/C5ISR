99b7dc5e0851df632277b5ff98e4f82a2c44cc11
Kyle Lehman <Kyle.Lehman@missionse.com>
2013-11-06 14:53:18 -0500

    Covered to use fragments

--- examples/MapsExample/src/com/missionse/mapsexample/MapsExampleActivity.java
+++ examples/MapsExample/src/com/missionse/mapsexample/MapsExampleActivity.java
@@ -1,15 +1,27 @@
 package com.missionse.mapsexample;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MapsExampleActivity extends Activity {
+public class MapsExampleActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_maps_example);
+		
+		FragmentManager fm = getFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		
+		if (fragment == null) {
+			fragment = new MapsFragment();
+			fm.beginTransaction()
+				.add(R.id.fragmentContainer, fragment)
+				.commit();
+		}
 	}
 
 	@Override
