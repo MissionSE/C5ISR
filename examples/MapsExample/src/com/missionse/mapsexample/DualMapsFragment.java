9766f128763e9e8510c1e523c0df687a6f0fa4f9
Kyle Lehman <Kyle.Lehman@missionse.com>
2013-11-21 16:16:19 -0500

    Change dual map to have mini map and added settings for colors.

--- examples/MapsExample/src/com/missionse/mapsexample/DualMapsFragment.java
+++ examples/MapsExample/src/com/missionse/mapsexample/DualMapsFragment.java
@@ -6,9 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Fragment;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -16,6 +19,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableLayout.LayoutParams;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
@@ -23,13 +28,15 @@ import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailed
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
@@ -37,12 +44,18 @@ import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.VisibleRegion;
 
 public class DualMapsFragment extends Fragment implements ConnectionCallbacks, OnConnectionFailedListener,
-LocationListener {
+LocationListener, OnSharedPreferenceChangeListener {
 	private static final LatLng MSE = new LatLng(39.974552, -74.976844);
 	private static final LatLng ZONE_A = new LatLng(39.974074, -74.977462);
 	private static final LatLng ZONE_B = new LatLng(39.975233, -74.977328);
 	private static final LatLng ZONE_C = new LatLng(39.975085, -74.976164);
-	private static final float MSE_BEARING = 27f;
+	private static final float MSE_BEARING = 0f;
+	private static final int DEF_FILL_COLOR = Color.argb(150, 0, 0, 0);
+
+	private static final String PREF_VIEW_AREA_FILL_COLOR = "pref_view_area_fill_color";
+	private static final String PREF_CIRCLE_FILL_COLOR = "pref_circle_fill_color";
+	private static final String PREF_MINI_MAP_SIZE = "pref_mini_map_size";
+	private static final float DEF_MINI_MAP_SIZE = 0.33f;
 
 	private GoogleMap mMapLeft;
 	private GoogleMap mMapRight;
@@ -50,6 +63,10 @@ LocationListener {
 	private Polygon mZoomedViewPolygon;
 
 	private LocationClient mLocationClient;
+
+	private SharedPreferences mPrefs;
+	
+	private ArrayList<Circle> mCircles = new ArrayList<Circle>();
 
 	// These settings are the same as the settings for the map. They will in fact give you updates
 	// at the maximal rates currently possible.
@@ -61,12 +78,15 @@ LocationListener {
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setHasOptionsMenu(true);
+		mPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		mPrefs.registerOnSharedPreferenceChangeListener(this);
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 		View v = inflater.inflate(R.layout.fragment_dual_maps, container, false);
+		
 
 		return v;
 	}
@@ -94,12 +114,14 @@ LocationListener {
 	@Override
 	public void onResume() {
 		super.onResume();
+		//		mPrefs.registerOnSharedPreferenceChangeListener(this);
 		setUpMapIfNeeded();
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
+		//		mPrefs.unregisterOnSharedPreferenceChangeListener(this);
 		if (mLocationClient != null) {
 			mLocationClient.disconnect();
 		}
@@ -107,11 +129,21 @@ LocationListener {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		boolean enabled = !item.isChecked();
 		switch (item.getItemId()) {
 		case R.id.myLocation:
-			boolean enabled = !item.isChecked();
 			item.setChecked(enabled);
 			setMyLocationEnabled(enabled);
+			return true;
+		case R.id.showMiniMap:
+			item.setChecked(enabled);
+			int visibility;
+			if (enabled) {
+				visibility = View.VISIBLE;
+			} else {
+				visibility = View.INVISIBLE;
+			}
+			getFragmentManager().findFragmentById(R.id.map_left).getView().setVisibility(visibility);
 			return true;
 		case R.id.resetMaps:
 			resetMaps();
@@ -123,7 +155,7 @@ LocationListener {
 
 	private void resetMaps() {
 		mMapLeft.animateCamera(CameraUpdateFactory
-				.newCameraPosition(new CameraPosition(MSE, 15f, 0, MSE_BEARING)));
+				.newCameraPosition(new CameraPosition(MSE, 12f, 0, MSE_BEARING)));
 		mMapRight.animateCamera(CameraUpdateFactory
 				.newCameraPosition(new CameraPosition(MSE, 18f, 0, MSE_BEARING)));
 	}
@@ -147,21 +179,13 @@ LocationListener {
 
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-//		super.onCreateOptionsMenu(menu, inflater);
+		//		super.onCreateOptionsMenu(menu, inflater);
 		inflater.inflate(R.menu.dual_maps_example, menu);
 		setMyLocationEnabled(menu.findItem(R.id.myLocation).isChecked());
 	}
 
 	private void setUpMapIfNeeded() {
 		// Do a null check to confirm that we have not already instantiated the map.
-		if (mMapLeft == null) {
-			// Try to obtain the map from the SupportMapFragment.
-			mMapLeft = ((MapFragment)getFragmentManager().findFragmentById(R.id.map_left)).getMap();
-			// Check if we were successful in obtaining the map.
-			if (mMapLeft != null) {
-				setUpLeftMap();
-			}
-		}
 		if (mMapRight == null) {
 			// Try to obtain the map from the SupportMapFragment.
 			mMapRight = ((MapFragment)getFragmentManager().findFragmentById(R.id.map_right)).getMap();
@@ -170,33 +194,24 @@ LocationListener {
 				setUpRightMap();
 			}
 		}
+		if (mMapLeft == null) {
+			// Try to obtain the map from the SupportMapFragment.
+			mMapLeft = ((MapFragment)getFragmentManager().findFragmentById(R.id.map_left)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (mMapLeft != null) {
+				setUpLeftMap();
+			}
+		}
 	}
 
 	private void setUpLeftMap() {
-		mMapLeft.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
-			@Override
-			public void onMapLoaded() {
-				mMapLeft.animateCamera(CameraUpdateFactory
-						.newCameraPosition(new CameraPosition(MSE, 15f, 0, MSE_BEARING)));
-			}
-		});
-		
+		mMapLeft.getUiSettings().setCompassEnabled(false);
+		mMapLeft.getUiSettings().setZoomControlsEnabled(false);
+
 		mMapLeft.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 			@Override
 			public void onMapClick(LatLng latLng) {
 				mMapRight.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-				drawZoomedViewPolygon();
-			}
-		});
-
-		mMapLeft.setOnCameraChangeListener(new OnCameraChangeListener() {
-			@Override
-			public void onCameraChange(CameraPosition posLeft) {
-				Log.d("Dual Maps Fragment", "posLeft.zoom=" + posLeft.zoom + ", mMapRight.zoom=" + mMapRight.getCameraPosition().zoom + ", mMapLeft.zoom=" + mMapLeft.getCameraPosition().zoom);
-				if (mMapRight.getCameraPosition().zoom < posLeft.zoom) {
-					mMapRight.animateCamera(CameraUpdateFactory.zoomTo(posLeft.zoom + 3));
-				}
-				drawZoomedViewPolygon();
 			}
 		});
 
@@ -204,12 +219,11 @@ LocationListener {
 			@Override
 			public boolean onMarkerClick(Marker marker) {
 				mMapRight.animateCamera(CameraUpdateFactory.newLatLng(marker.getPosition()));
-				drawZoomedViewPolygon();
 				return false;
 			}
 		});
 
-		mMapLeft.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+		mMapLeft.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 
 		mMapLeft.addMarker(new MarkerOptions()
 		.position(ZONE_A));
@@ -222,6 +236,7 @@ LocationListener {
 	}
 
 	private void setUpRightMap() {
+		mMapRight.getUiSettings().setCompassEnabled(true);
 		mMapRight.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
 			@Override
 			public void onMapLoaded() {
@@ -230,47 +245,52 @@ LocationListener {
 			}
 		});
 
-		mMapRight.setOnCameraChangeListener(new OnCameraChangeListener() {
+		mMapRight.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+
 			@Override
-			public void onCameraChange(CameraPosition posRight) {
-				if (mMapLeft.getCameraPosition().zoom > posRight.zoom) {
-					mMapLeft.animateCamera(CameraUpdateFactory.zoomTo(posRight.zoom - 3));
-				}
-				drawZoomedViewPolygon();
+			public void onCameraChange(CameraPosition arg0) {
+				animateZoomedView();
 			}
 		});
 
 		mMapRight.setMapType(MAP_TYPE_HYBRID);
 
 		double radius = 50;
-		int fillColor = Color.argb(75, 255, 0, 0);
+		int fillColor = mPrefs.getInt(PREF_CIRCLE_FILL_COLOR, DEF_FILL_COLOR);
 		int strokeColor = Color.RED;
 		float strokeWidth = 2f;
 
-		mMapRight.addCircle(new CircleOptions()
+		mCircles.add(mMapRight.addCircle(new CircleOptions()
 		.center(ZONE_A)
 		.radius(radius)
 		.fillColor(fillColor)
 		.strokeWidth(strokeWidth)
-		.strokeColor(strokeColor));
+		.strokeColor(strokeColor)));
 
-		mMapRight.addCircle(new CircleOptions()
+		mCircles.add(mMapRight.addCircle(new CircleOptions()
 		.center(ZONE_B)
 		.radius(radius)
 		.fillColor(fillColor)
 		.strokeWidth(strokeWidth)
-		.strokeColor(strokeColor));
+		.strokeColor(strokeColor)));
 
-		mMapRight.addCircle(new CircleOptions()
+		mCircles.add(mMapRight.addCircle(new CircleOptions()
 		.center(ZONE_C)
 		.radius(radius)
 		.fillColor(fillColor)
 		.strokeWidth(strokeWidth)
-		.strokeColor(strokeColor));
+		.strokeColor(strokeColor)));
+	}
+
+	private void animateZoomedView() {
+		LatLngBounds rightBounds = mMapRight.getProjection().getVisibleRegion().latLngBounds;
+		CameraUpdate leftCameraUpdate = CameraUpdateFactory.newLatLngBounds(rightBounds, 50);
+		mMapLeft.animateCamera(leftCameraUpdate);
+
+		drawZoomedViewPolygon();
 	}
 
 	private void drawZoomedViewPolygon() {
-		int fillColor = Color.argb(150, 0, 0, 0);
 		float strokeWidth = 0f;
 		int strokeColor = Color.GRAY;
 
@@ -282,17 +302,43 @@ LocationListener {
 		rightVRpoints.add(rightVR.nearLeft);
 
 		if (mZoomedViewPolygon == null) {
+			int fillColor = mPrefs.getInt(PREF_VIEW_AREA_FILL_COLOR, DEF_FILL_COLOR);
 			mZoomedViewPolygon = mMapLeft.addPolygon(new PolygonOptions()
 			.addAll(rightVRpoints)
 			.fillColor(fillColor)
 			.strokeWidth(strokeWidth)
 			.strokeColor(strokeColor));
 		} else {
-			mZoomedViewPolygon.setFillColor(fillColor);
-			mZoomedViewPolygon.setStrokeWidth(strokeWidth);
 			mZoomedViewPolygon.setPoints(rightVRpoints);
-			mZoomedViewPolygon.setStrokeColor(strokeColor);
 		}
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		if (PREF_VIEW_AREA_FILL_COLOR.equals(key)) {
+			mZoomedViewPolygon.setFillColor(sharedPreferences.getInt(PREF_VIEW_AREA_FILL_COLOR, DEF_FILL_COLOR));
+		} else if (PREF_CIRCLE_FILL_COLOR.equals(key)) {
+			for (Circle circle : mCircles) {
+				circle.setFillColor(sharedPreferences.getInt(PREF_CIRCLE_FILL_COLOR, DEF_FILL_COLOR));
+			}
+		}
+	}
+	
+	private void setupViewWeights(View v) {
+		float weight = mPrefs.getFloat(PREF_MINI_MAP_SIZE, DEF_MINI_MAP_SIZE);
+		v.findViewById(R.id.spacer_1).setLayoutParams(
+				new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT, 
+						LayoutParams.MATCH_PARENT, 
+						weight));
+		v.findViewById(R.id.spacer_2).setLayoutParams(
+				new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT, 
+						LayoutParams.MATCH_PARENT, 
+						weight));
+		v.findViewById(R.id.map_left).setLayoutParams(
+				new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT, 
+						LayoutParams.MATCH_PARENT, 
+						weight));
 	}
 
 }
