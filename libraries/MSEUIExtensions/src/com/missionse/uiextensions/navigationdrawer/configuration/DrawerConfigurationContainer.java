956aefb9fad3f8dd8e30b8926464f642c6125e30
jsantangelo <jamesfsantangelo@gmail.com>
2013-12-09 15:03:50 -0500

    Fixed checkstyle errors in new library code.

--- libraries/MSEUIExtensions/src/com/missionse/uiextensions/navigationdrawer/configuration/DrawerConfigurationContainer.java
+++ libraries/MSEUIExtensions/src/com/missionse/uiextensions/navigationdrawer/configuration/DrawerConfigurationContainer.java
@@ -2,8 +2,14 @@ package com.missionse.uiextensions.navigationdrawer.configuration;
 
 import java.util.HashMap;
 
+/**
+ * Contains configurations for all possible potential Navigation drawers.
+ */
 public class DrawerConfigurationContainer {
 
+	/**
+	 * Denotes which the side of the application on which the drawer will appear.
+	 */
 	public enum DrawerType {
 		LEFT, RIGHT
 	}
@@ -12,28 +18,54 @@ public class DrawerConfigurationContainer {
 	private int mMainLayout;
 	private int mDrawerLayout;
 
+	/**
+	 * Creates a new DrawerConfigurationContainer, to hold DrawerConfigurations and overall drawer configuration.
+	 * @param mainLayout the layout of the activity that contains the drawer layout
+	 * @param drawerLayout the id of the drawer layout
+	 */
 	public DrawerConfigurationContainer(final int mainLayout, final int drawerLayout) {
 		mMainLayout = mainLayout;
 		mDrawerLayout = drawerLayout;
 	}
 
+	/**
+	 * Saves the configuration for a given Drawer.
+	 * @param type which side this configuration applies to
+	 * @param configuration the configuration
+	 */
 	public void setConfiguration(final DrawerType type, final DrawerConfiguration configuration) {
 
 		mConfigurations.put(type, configuration);
 	}
 
+	/**
+	 * Retrieves the configuration for the left-hand drawer.
+	 * @return the configuration
+	 */
 	public DrawerConfiguration getLeftConfiguration() {
 		return mConfigurations.get(DrawerType.LEFT);
 	}
 
+	/**
+	 * Retrieves the configuration for the right-hand drawer.
+	 * @return the configuration
+	 */
 	public DrawerConfiguration getRightConfiguration() {
 		return mConfigurations.get(DrawerType.RIGHT);
 	}
 
+	/**
+	 * Retrieves the resource ID of the main layout.
+	 * @return the resource ID
+	 */
 	public int getMainLayout() {
 		return mMainLayout;
 	}
 
+	/**
+	 * Retrieves the resource ID of the drawer layout.
+	 * @return the resource ID
+	 */
 	public int getDrawerLayout() {
 		return mDrawerLayout;
 	}
