23182f9a6b671a3f0f978f923fa4cd33ce27853c
Kyle <kyle.h.lehman@gmail.com>
2013-12-14 14:27:24 -0500

    Added main activity to display a list of multiple examples to demo.

---
+++ examples/MapsExample/src/com/missionse/mapsexample/MainActivity.java
@@ -0,0 +1,39 @@
+package com.missionse.mapsexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private ViewGroup mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        mListView = (ViewGroup) findViewById(R.id.list);
+
+        addDemo("Maps Example", MapsExampleActivity.class);
+        addDemo("Maps Overview", OverviewMapsActivity.class);
+    }
+
+    private void addDemo(String demoName, Class<? extends Activity> activityClass) {
+        Button b = new Button(this);
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        b.setLayoutParams(layoutParams);
+        b.setText(demoName);
+        b.setTag(activityClass);
+        b.setOnClickListener(this);
+        mListView.addView(b);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Class activityClass = (Class) view.getTag();
+        startActivity(new Intent(this, activityClass));
+    }
+}
