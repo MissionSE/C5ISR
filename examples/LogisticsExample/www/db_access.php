501860356138242b4a8a292d0f328cfcf55730e0
Mike Testen <mrtesten@gmail.com>
2013-12-16 15:41:35 -0500

    Cleaned up insert item in database script.

--- examples/LogisticsExample/www/db_access.php
+++ examples/LogisticsExample/www/db_access.php
@@ -1,19 +1,19 @@
 <?php
 	class DatabaseAccessor {
-		function __construct() {
+		public function __construct() {
 			static $db;
 
 			require_once __DIR__ . '/db_connect.php';
 			$db = new DatabaseConnector();
 		}
 
-		function query_database($query) {
+		public function query_database($query) {
 			$result = mysql_query($query) or die(mysql_error());
 			
 			return $result;
 		}
 
-		function fetch_table($tablename, $columns) {
+		public function fetch_table($tablename, $columns) {
 			$result = $this->query_database("SELECT * FROM $tablename");
 
 			$response = array();
@@ -38,31 +38,40 @@
 			return $response;
 		}
 
-		function insert_item($tablename, $columns) {
-			$response = array();
-			$success = 0;
-
-			$columns_posted = true;
-			$columns_string = "";
-			$values_string = "";
-			foreach ($columns as $column) {
-				if (isset($_POST[$column])) {
-					$value = mysql_real_escape_string($_POST["$column"]);
-					if ($columns_string != "" && $values_string != "") {
-						$columns_string = "$columns_string, ";
-						$values_string = "$values_string, ";
-					}
-					$columns_string = "$columns_string$column";
-					$values_string = "$values_string'$value'";
-				} else {
-					$columns_posted = false;
-					$response["message"] = "Required field(s) missing.";
-					break;
+		private function get_post_data($keys) {
+			$post_data = array();
+			foreach ($keys as $key) {
+				if (isset($_POST[$key])) {
+					$post_data[$key] = mysql_real_escape_string($_POST[$key]);
 				}
 			}
 
-			if ($columns_posted) {
-				$result = $this->query_database("INSERT INTO $tablename($columns_string) VALUES($values_string)");
+			return $post_data;
+		}
+
+		private function build_insert_query($tablename, $insert_data) {
+			$columns_string = "";
+			$values_string = "";
+			foreach ($insert_data as $column => $value) {
+				if ($columns_string != "" && $values_string != "") {
+					$columns_string = "$columns_string, ";
+					$values_string = "$values_string, ";
+				}
+				$columns_string = "$columns_string$column";
+				$values_string = "$values_string'$value'";
+			}
+
+			return "INSERT INTO $tablename($columns_string) VALUES($values_string)";
+		}
+
+		public function insert_item($tablename, $columns) {
+			$response = array();
+			$success = 0;
+
+			$post_data = $this->get_post_data($columns);
+			if (count($columns) == count($post_data)) {
+				$query = $this->build_insert_query($tablename, $post_data);
+				$result = $this->query_database($query);
 				if ($result) {
 					$id = mysql_insert_id();
 					if ($id) {
@@ -74,6 +83,8 @@
 				} else {
 					$response["message"] = mysql_error();
 				}
+			} else {
+				$response["message"] = "Required field(s) missing.";
 			}
 
 			$response["success"] = $success;
