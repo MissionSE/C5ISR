8aaef271164e32bf5c9479cace2675e1671a3060
Mike Testen <mrtesten@gmail.com>
2013-12-17 12:44:03 -0500

    Refactored edit location script so it can be used in the logistics example.

--- examples/MapDatabaseExample/www/db_access.php
+++ examples/MapDatabaseExample/www/db_access.php
@@ -31,9 +31,9 @@
 				}
 			}
 
-			$response["success"] = $success;
+			$response['success'] = $success;
 			if (!$success) {
-				$response["message"] = "Table $tablename is empty.";
+				$response['message'] = "Table $tablename is empty.";
 			}
 			return $response;
 		}
@@ -50,10 +50,10 @@
 		}
 
 		private function build_insert_query($tablename, $insert_data) {
-			$columns_string = "";
-			$values_string = "";
+			$columns_string = '';
+			$values_string = '';
 			foreach ($insert_data as $column => $value) {
-				if ($columns_string != "" && $values_string != "") {
+				if ($columns_string != '' && $values_string != '') {
 					$columns_string = "$columns_string, ";
 					$values_string = "$values_string, ";
 				}
@@ -75,19 +75,19 @@
 				if ($result) {
 					$id = mysql_insert_id();
 					if ($id) {
-						$response["id"] = $id;
+						$response['id'] = $id;
 						$success = 1;
 					} else {
-						$response["message"] = "Unable to get last insert id.";
+						$response['message'] = 'Unable to get last insert id.';
 					}
 				} else {
-					$response["message"] = mysql_error();
+					$response['message'] = mysql_error();
 				}
 			} else {
-				$response["message"] = "Required field(s) missing.";
+				$response['message'] = 'Required field(s) missing.';
 			}
 
-			$response["success"] = $success;
+			$response['success'] = $success;
 			return $response;
 		}
 
@@ -95,21 +95,53 @@
 			$response = array();
 			$success = 0;
 
-			$post_data = $this->get_post_data(["id"]);
-			$id = $post_data["id"];
+			$id = $this->get_post_data(['id'])['id'];
 			if ($id) {
 				$query = "DELETE FROM $tablename WHERE id='$id'";
 				$result = $this->query_database($query);
 				if (mysql_affected_rows() > 0) {
 					$success = 1;
 				} else {
-					$response["message"] = "Item with id $id not found.";
+					$response['message'] = "Item with id $id not found.";
 				}
 			} else {
-				$response["message"] = "Required field(s) missing.";
+				$response['message'] = 'Required field(s) missing.';
 			}
 
-			$response["success"] = $success;
+			$response['success'] = $success;
+			return $response;
+		}
+
+		private function build_update_query($tablename, $id, $update_data) {
+			$field_string = '';
+			foreach ($update_data as $key => $value) {
+				if ($field_string != '') {
+					$field_string = "$field_string, ";
+				}
+				$field_string = "$field_string$key='$value'";
+			}
+			return "UPDATE $tablename SET $field_string, updated_at=NOW() WHERE id='$id'";
+		}
+
+		public function update_item($tablename, $columns) {
+			$response = array();
+			$success = 0;
+
+			$id = $this->get_post_data(['id'])['id'];
+			$post_data = $this->get_post_data($columns);
+			if ($id && (count($columns) == count($post_data))) {
+				$query = $this->build_update_query($tablename, $id, $post_data);
+				$result = mysql_query($query);
+				if ($result) {
+					$success = 1;
+				} else {
+					$response['message'] = "Item with id $id not found.";
+				}
+			} else {
+				$response['message'] = 'Required field(s) missing.';
+			}
+
+			$response['success'] = $success;
 			return $response;
 		}
 	}
--- examples/MapDatabaseExample/www/edit_location.php
+++ examples/MapDatabaseExample/www/edit_location.php
@@ -1,31 +1,10 @@
 <?php
-	$response = array();
+	$tablename = "locations";
+	$columns = array("name", "latitude", "longitude");
 
-	if (isset($_POST['id']) && isset($_POST['name']) && isset($_POST['latitude']) && isset($_POST['longitude'])) {
-		$id = mysql_real_escape_string($_POST['id']);
-		$name = mysql_real_escape_string($_POST['name']);
-		$latitude = mysql_real_escape_string($_POST['latitude']);
-		$longitude = mysql_real_escape_string($_POST['longitude']);
+	require_once __DIR__ . '/db_access.php';
+	$db = new DatabaseAccessor();
+	$response = $db->update_item($tablename, $columns);
 
-		require_once __DIR__ . '/db_connect.php';
-		$db = new DatabaseConnector();
-		$result = mysql_query("UPDATE locations SET name='$name', latitude='$latitude', longitude='$longitude', updated_at=NOW() WHERE id='$id'");
-
-		if ($result) {
-			$response["success"] = 1;
-			$response["message"] = "Location successfully modified";
-
-			echo json_encode($response);
-		} else {
-			$response["success"] = 0;
-			$response["message"] = mysql_error();
-
-			echo json_encode($response);
-		}
-	} else {
-		$response["success"] = 0;
-		$response["message"] = "Required field(s) is missing";
-
-		echo json_encode($response);
-	}
+	echo json_encode($response);
 ?>
