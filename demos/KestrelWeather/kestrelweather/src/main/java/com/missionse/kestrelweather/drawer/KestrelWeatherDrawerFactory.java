1c79f3511862063bbf20e9ffc383fb1451a2985e
jsantangelo <jamesfsantangelo@gmail.com>
2014-02-06 13:55:59 -0500

    Added basic items to the Navigation Drawer for the Kestrel Weather app.

--- demos/KestrelWeather/kestrelweather/src/main/java/com/missionse/kestrelweather/drawer/KestrelWeatherDrawerFactory.java
+++ demos/KestrelWeather/kestrelweather/src/main/java/com/missionse/kestrelweather/drawer/KestrelWeatherDrawerFactory.java
@@ -15,8 +15,10 @@ import java.util.List;
 
 public class KestrelWeatherDrawerFactory {
 
-	public static final int MAP_OVERVIEW = 101;
-	//public static final int REPORT_DATABASE = 102;
+    public static final int MAP_OVERVIEW = 101;
+    public static final int CREATE_REPORT = 102;
+    public static final int REPORT_SYNC = 103;
+    public static final int REPORT_DATABASE = 104;
 
 	private Context mContext;
 
@@ -50,8 +52,14 @@ public class KestrelWeatherDrawerFactory {
 	public void addNavigationMenuItems(final DrawerAdapter adapter) {
 		List<DrawerItem> menu = new ArrayList<DrawerItem>();
 
-		menu.add(DrawerSimpleItem.create(MAP_OVERVIEW, mContext.getResources().getString(R.string.drawer_overview),
-				0, true));
+        menu.add(DrawerSimpleItem.create(MAP_OVERVIEW, mContext.getResources().getString(R.string.drawer_overview),
+            0, true));
+        menu.add(DrawerSimpleItem.create(CREATE_REPORT, mContext.getResources().getString(R.string.drawer_create_report),
+            0, true));
+        menu.add(DrawerSimpleItem.create(REPORT_SYNC, mContext.getResources().getString(R.string.drawer_sync_reports),
+            0, true));
+        menu.add(DrawerSimpleItem.create(REPORT_DATABASE, mContext.getResources().getString(R.string.drawer_report_database),
+            0, true));
 
 		for (DrawerItem item : menu) {
 			adapter.add(item);
