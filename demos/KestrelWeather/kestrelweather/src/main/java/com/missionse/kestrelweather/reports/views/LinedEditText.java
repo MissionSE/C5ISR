d99cf1d4ac227a8047b4300f7a83847746786cc8
rvieras <rvieras3@gmail.com>
2014-02-07 12:33:19 -0500

    Changes to KestrelWeather Demo. Templates for note viewing and editing.

---
+++ demos/KestrelWeather/kestrelweather/src/main/java/com/missionse/kestrelweather/reports/views/LinedEditText.java
@@ -0,0 +1,45 @@
+package com.missionse.kestrelweather.reports.views;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+/**
+ * Custome view to simulate lined paper
+ */
+public class LinedEditText extends EditText {
+	private Rect mRect;
+	private Paint mPaint;
+
+	/**
+	 * Constructor.
+	 * @param context - Context associated with this view
+	 * @param attrs - AttributeSet that configures how this view will look.
+	 */
+	public LinedEditText(Context context, AttributeSet attrs) {
+		super(context, attrs);
+
+		mRect = new Rect();
+		mPaint = new Paint();
+		mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+		mPaint.setColor(Color.BLACK);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		int height = canvas.getHeight();
+		int curHeight = 0;
+		Rect r = mRect;
+		Paint paint = mPaint;
+		int baseline = getLineBounds(0, r);
+		for (curHeight = baseline + 1; curHeight < height;
+			 curHeight += getLineHeight()) {
+			canvas.drawLine(r.left, curHeight, r.right, curHeight, paint);
+		}
+		super.onDraw(canvas);
+	}
+}
