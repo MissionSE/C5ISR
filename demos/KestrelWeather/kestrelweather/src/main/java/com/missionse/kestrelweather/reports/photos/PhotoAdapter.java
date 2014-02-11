9d21d346598feef6204a49b49ab56be1ef8a0b2b
Mike Testen <mrtesten@gmail.com>
2014-02-11 15:55:38 -0500

    Cleaned up the photo overview fragment.

--- demos/KestrelWeather/kestrelweather/src/main/java/com/missionse/kestrelweather/reports/photos/PhotoAdapter.java
+++ demos/KestrelWeather/kestrelweather/src/main/java/com/missionse/kestrelweather/reports/photos/PhotoAdapter.java
@@ -1,54 +1,144 @@
 package com.missionse.kestrelweather.reports.photos;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.missionse.kestrelweather.R;
 
-public class PhotoAdapter extends BaseAdapter {
-	private Context mContext;
+import java.util.ArrayList;
+import java.util.List;
 
-	private Integer[] mImageIds = {
-			R.drawable.ic_launcher,
-			R.drawable.camera
-	};
+/**
+ * Provides an adapter for a list of photos.
+ */
+public class PhotoAdapter extends ArrayAdapter<Uri> {
+	private static final String TAG = PhotoAdapter.class.getName();
+	private static final int BYTE_SIZE = 1024;
+	private final int mResource;
+	private List<Uri> mSelectedPhotos;
 
-	public PhotoAdapter(final Context context) {
-		mContext = context;
-	}
-	@Override
-	public int getCount() {
-		return mImageIds.length;
-	}
-
-	@Override
-	public Object getItem(int position) {
-		return mImageIds[position];
-	}
-
-	@Override
-	public long getItemId(int position) {
-		return 0;
+	/**
+	 * Constructor.
+	 * @param context The current context.
+	 * @param resource The resource ID for a layout file containing a TextView to use when instantiating views.
+	 */
+	public PhotoAdapter(final Context context, final int resource) {
+		super(context, resource);
+		mResource = resource;
+		mSelectedPhotos = new ArrayList<Uri>();
 	}
 
 	@Override
 	public View getView(int position, View view, ViewGroup viewGroup) {
-		ImageView imageView;
 		if (view == null) {
-			imageView = new ImageView(mContext);
-			imageView.setLayoutParams(new GridView.LayoutParams(
-					(int) mContext.getResources().getDimension(R.dimen.photo_width),
-					(int) mContext.getResources().getDimension(R.dimen.photo_height)));
-			imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-		} else {
-			imageView = (ImageView) view;
+			LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			view = layoutInflater.inflate(mResource, null);
 		}
 
-		imageView.setImageResource(mImageIds[position]);
-		return imageView;
+		if (view != null) {
+			ImageView thumbnailView = (ImageView) view.findViewById(R.id.photo_overview_item_thumbnail);
+			TextView imageFilenameView = (TextView) view.findViewById(R.id.photo_overview_item_file_name);
+			TextView imageSizeView = (TextView) view.findViewById(R.id.photo_overview_item_file_size);
+
+			Uri uri = getItem(position);
+			Cursor cursor = getContext().getContentResolver()
+					.query(uri, null, null, null, null, null);
+			try {
+				if (cursor != null && cursor.moveToFirst()) {
+					if (thumbnailView != null) {
+						long id = getUriId(cursor.getString(
+								cursor.getColumnIndex(DocumentsContract.Root.COLUMN_DOCUMENT_ID)));
+						if (id != -1) {
+							Bitmap thumbnail = MediaStore.Images.Thumbnails.getThumbnail(
+									getContext().getContentResolver(), id, MediaStore.Images.Thumbnails.MICRO_KIND, null);
+							thumbnailView.setImageBitmap(thumbnail);
+						} else {
+							thumbnailView.setImageResource(R.drawable.ic_launcher);
+						}
+					}
+
+					if (imageFilenameView != null) {
+						String displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+						imageFilenameView.setText(displayName);
+					}
+
+					if (imageSizeView != null) {
+						int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
+						if (!cursor.isNull(sizeIndex)) {
+							long size = cursor.getLong(sizeIndex);
+							imageSizeView.setText(humanReadableByteCount(size));
+						} else {
+							imageSizeView.setText(getContext().getString(R.string.unknown));
+						}
+					}
+				}
+			} finally {
+				if (cursor != null) {
+					cursor.close();
+				}
+			}
+
+			if (mSelectedPhotos.contains(uri)) {
+				view.setBackgroundColor(getContext().getResources().getColor(R.color.gray_light));
+			} else {
+				view.setBackgroundColor(getContext().getResources().getColor(R.color.background_holo_light));
+			}
+		}
+		return view;
+	}
+
+	/**
+	 * Sets the selected status of a URI.
+	 * @param uri The uri item.
+	 * @param selected Whether the uri is selected.
+	 */
+	public void setSelected(final Uri uri, final boolean selected) {
+		Log.d(TAG, "setSelected: " + selected + " - " + uri.toString());
+		if (selected) {
+			mSelectedPhotos.add(uri);
+		} else {
+			mSelectedPhotos.remove(uri);
+		}
+	}
+
+	/**
+	 * Clear all selected items.
+	 */
+	public void clearSelected() {
+		mSelectedPhotos.clear();
+	}
+
+	private long getUriId(final String uri) {
+		long uriId = -1;
+		String[] uriSegments = uri.split(":");
+		try {
+			uriId = Long.valueOf(uriSegments[uriSegments.length - 1]);
+		} catch (NumberFormatException exception) {
+			Log.d(TAG, "ID was not valid long: " + uri);
+		}
+
+		return uriId;
+	}
+
+	private String humanReadableByteCount(long bytes) {
+		if (bytes < BYTE_SIZE) {
+			return bytes + " B";
+		}
+
+		int exp = (int) (Math.log(bytes) / Math.log(BYTE_SIZE));
+		String pre = ("KMGTPE").charAt(exp - 1) + "";
+		return String.format("%.1f %sB", bytes / Math.pow(BYTE_SIZE, exp), pre);
 	}
 }
--- demos/KestrelWeather/kestrelweather/src/main/java/com/missionse/kestrelweather/reports/photos/PhotoOverviewFragment.java
+++ demos/KestrelWeather/kestrelweather/src/main/java/com/missionse/kestrelweather/reports/photos/PhotoOverviewFragment.java
@@ -2,38 +2,33 @@ package com.missionse.kestrelweather.reports.photos;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.graphics.PorterDuff;
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.missionse.kestrelweather.R;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A fragment used to manage the photos attached to a report.
  */
-public class PhotoOverviewFragment extends Fragment implements ActionMode.Callback, AdapterView.OnItemClickListener {
+public class PhotoOverviewFragment extends Fragment {
+	private static final int READ_REQUEST_CODE = 100;
+
 	private PhotoAdapter mPhotoAdapter;
 	private Activity mActivity;
-	private ActionMode mActionMode;
-	private List<ImageView> mSelectedPhotos;
 
 	/**
 	 * Constructor.
 	 */
 	public PhotoOverviewFragment() {
-		mSelectedPhotos = new ArrayList<ImageView>();
 	}
 
 	@Override
@@ -58,11 +53,11 @@ public class PhotoOverviewFragment extends Fragment implements ActionMode.Callba
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View contentView = inflater.inflate(R.layout.fragment_photo_overview, container, false);
 		if (contentView != null) {
-			GridView photoGrid = (GridView) contentView.findViewById(R.id.photo_grid_view);
+			ListView mPhotoList = (ListView) contentView.findViewById(R.id.photo_overview_list);
 
-			mPhotoAdapter = new PhotoAdapter(mActivity);
-			photoGrid.setAdapter(mPhotoAdapter);
-			photoGrid.setOnItemClickListener(this);
+			mPhotoAdapter = new PhotoAdapter(mActivity, R.layout.fragment_photo_overview_item);
+			mPhotoList.setAdapter(mPhotoAdapter);
+			mPhotoList.setMultiChoiceModeListener(new PhotoMultiChoiceModeListener(mActivity, mPhotoList, mPhotoAdapter));
 		}
 
 		return contentView;
@@ -76,57 +71,36 @@ public class PhotoOverviewFragment extends Fragment implements ActionMode.Callba
 
 	@Override
 	public boolean onOptionsItemSelected(final MenuItem item) {
+		if (item.getItemId() == R.id.action_add_photo) {
+			Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+			intent.addCategory(Intent.CATEGORY_OPENABLE);
+			intent.setType("image/*");
+			intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+			startActivityForResult(intent, READ_REQUEST_CODE);
+			return true;
+		}
 		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
-	public boolean onPrepareActionMode(final ActionMode actionMode, final Menu menu) {
-		return false;
-	}
+	public void onActivityResult(final int requestCode, final int resultCode, final Intent resultData) {
+		super.onActivityResult(requestCode, resultCode, resultData);
 
-	@Override
-	public boolean onCreateActionMode(final ActionMode actionMode, final Menu menu) {
-		mActionMode = actionMode;
-
-		MenuInflater menuInflater = mActionMode.getMenuInflater();
-		if (menuInflater != null) {
-			menuInflater.inflate(R.menu.photo_overview_action_bar, menu);
-		}
-		return true;
-	}
-
-	@Override
-	public boolean onActionItemClicked(final ActionMode actionMode, final MenuItem menuItem) {
-		return false;
-	}
-
-	@Override
-	public void onDestroyActionMode(final ActionMode actionMode) {
-		mActionMode = null;
-		for (ImageView selectedPhoto : mSelectedPhotos) {
-			selectedPhoto.clearColorFilter();
-		}
-		mSelectedPhotos.clear();
-	}
-
-	@Override
-	public void onItemClick(final AdapterView<?> adapterView, final View view, final int position, final long id) {
-		ImageView imageView = (ImageView) view;
-		if (mSelectedPhotos.contains(imageView)) {
-			mSelectedPhotos.remove(imageView);
-			imageView.clearColorFilter();
-
-			if (mSelectedPhotos.size() == 0 && mActionMode != null) {
-				mActionMode.finish();
-			}
-		} else {
-			mSelectedPhotos.add(imageView);
-			if (mActivity != null) {
-				imageView.setColorFilter(
-						mActivity.getResources().getColor(R.color.holo_orange_light),
-						PorterDuff.Mode.MULTIPLY);
-				if (mActionMode == null) {
-					mActivity.startActionMode(this);
+		if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+			if (resultData != null) {
+				Uri uri = resultData.getData();
+				if (uri != null) {
+					mPhotoAdapter.add(uri);
+				} else {
+					ClipData clipData = resultData.getClipData();
+					if (clipData != null) {
+						for (int index = 0; index < clipData.getItemCount(); ++index) {
+							ClipData.Item item = clipData.getItemAt(index);
+							if (item != null) {
+								mPhotoAdapter.add(item.getUri());
+							}
+						}
+					}
 				}
 			}
 		}
