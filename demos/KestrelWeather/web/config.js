c632986d331cbd0750566576a93828200d9ac02f
jsantangelo <jamesfsantangelo@gmail.com>
2014-02-27 11:00:40 -0500

    Changing selected port of Kestrel Weather server app to a non-standard Express/NodeJS port.

--- demos/KestrelWeather/web/config.js
+++ demos/KestrelWeather/web/config.js
@@ -2,10 +2,10 @@ var path = require('path');
 var express = require('express');
 
 var applyConfiguration = function(server) {
-	server.set('port', process.env.PORT || 3000);
+	server.set('port', process.env.PORT || 3009);
 	server.use(express.favicon());
 	server.use(express.logger('dev'));
-	
+
 	server.use(server.router);
 	server.use(express.json())
 		.use(express.urlencoded());
@@ -13,7 +13,7 @@ var applyConfiguration = function(server) {
 	server.use(express.methodOverride());
 	server.use(express.cookieParser('kestrel'));
 	server.use(express.session());
-	
+
 	server.use(express.static(path.join(__dirname, 'public')));
 
 	// development only
