8d8f42deedd845825d15433171c3f514c9a7ba09
Kyle <kyle.h.lehman@gmail.com>
2014-03-04 11:45:40 -0500

    More Kestrel Weather preference cleanup - Converted all keys to string resources - Removed unused preferences - Cleaned up ScrollView UI in license info

--- demos/KestrelWeather/kestrelweather/src/main/java/com/missionse/kestrelweather/preferences/SettingsActivity.java
+++ demos/KestrelWeather/kestrelweather/src/main/java/com/missionse/kestrelweather/preferences/SettingsActivity.java
@@ -1,9 +1,8 @@
 package com.missionse.kestrelweather.preferences;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.ListPreference;
@@ -11,8 +10,6 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
-import android.text.TextUtils;
 
 import com.missionse.kestrelweather.R;
 
@@ -23,19 +20,13 @@ import java.util.List;
  * handset devices, settings are presented as a single list. On tablets,
  * settings are split by category, with category headers shown to the left of
  * the list of settings.
- * <p>
+ * <p/>
  * See <a href="http://developer.android.com/design/patterns/settings.html">
  * Android Design: Settings</a> for design guidelines and the <a
  * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
  * API Guide</a> for more information on developing a Settings UI.
  */
 public class SettingsActivity extends PreferenceActivity {
-
-	/** {@inheritDoc} */
-	@Override
-	public void onBuildHeaders(List<Header> target) {
-		loadHeadersFromResource(R.xml.pref_headers, target);
-	}
 
 	/**
 	 * A preference value change listener that updates the preference's summary
@@ -57,29 +48,6 @@ public class SettingsActivity extends PreferenceActivity {
 						index >= 0
 								? listPreference.getEntries()[index]
 								: null);
-
-			} else if (preference instanceof RingtonePreference) {
-				// For ringtone preferences, look up the correct display value
-				// using RingtoneManager.
-				if (TextUtils.isEmpty(stringValue)) {
-					// Empty values correspond to 'silent' (no ringtone).
-					preference.setSummary(R.string.pref_ringtone_silent);
-
-				} else {
-					Ringtone ringtone = RingtoneManager.getRingtone(
-							preference.getContext(), Uri.parse(stringValue));
-
-					if (ringtone == null) {
-						// Clear the summary if there was a lookup error.
-						preference.setSummary(null);
-					} else {
-						// Set the summary to reflect the new ringtone display
-						// name.
-						String name = ringtone.getTitle(preference.getContext());
-						preference.setSummary(name);
-					}
-				}
-
 			} else {
 				// For all other preferences, set the summary to the value's
 				// simple string representation.
@@ -88,28 +56,6 @@ public class SettingsActivity extends PreferenceActivity {
 			return true;
 		}
 	};
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-
-	}
-
-	/**
-	 * Subclasses should override this method and verify that the given fragment is a valid type
-	 * to be attached to this activity. The default implementation returns <code>true</code> for
-	 * apps built for <code>android:targetSdkVersion</code> older than
-	 * {@link android.os.Build.VERSION_CODES#KITKAT}. For later versions, it will throw an exception.
-	 *
-	 * @param fragmentName the class name of the Fragment about to be attached to this activity.
-	 * @return true if the fragment class name is valid for this Activity and false otherwise.
-	 */
-	@Override
-	protected boolean isValidFragment(String fragmentName) {
-		return true;
-	}
 
 	/**
 	 * Binds a preference's summary to its value. More specifically, when the
@@ -132,9 +78,34 @@ public class SettingsActivity extends PreferenceActivity {
 						.getString(preference.getKey(), ""));
 	}
 
+	@Override
+	public void onBuildHeaders(List<Header> target) {
+		loadHeadersFromResource(R.xml.pref_headers, target);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
 	/**
-	 * This fragment shows general preferences only. It is used when the
-	 * activity is showing a two-pane settings UI.
+	 * Subclasses should override this method and verify that the given fragment is a valid type
+	 * to be attached to this activity. The default implementation returns <code>true</code> for
+	 * apps built for <code>android:targetSdkVersion</code> older than
+	 * {@link android.os.Build.VERSION_CODES#KITKAT}. For later versions, it will throw an exception.
+	 *
+	 * @param fragmentName the class name of the Fragment about to be attached to this activity.
+	 * @return true if the fragment class name is valid for this Activity and false otherwise.
+	 */
+	@Override
+	protected boolean isValidFragment(String fragmentName) {
+		return true;  //FIXME
+	}
+
+	/**
+	 * This fragment shows units preferences only.
 	 */
 	public static class UnitsPreferenceFragment extends PreferenceFragment {
 		@Override
@@ -146,52 +117,10 @@ public class SettingsActivity extends PreferenceActivity {
 			// to their values. When their values change, their summaries are
 			// updated to reflect the new value, per the Android Design
 			// guidelines.
-			bindPreferenceSummaryToValue(findPreference("com_missionse_kestrelweather_temperature_units"));
-			bindPreferenceSummaryToValue(findPreference("com_missionse_kestrelweather_windspeed_units"));
-			bindPreferenceSummaryToValue(findPreference("com_missionse_kestrelweather_pressure_units"));
-			bindPreferenceSummaryToValue(findPreference("com_missionse_kestrelweather_rainfall_units"));
-		}
-	}
-
-	/**
-	 * This fragment shows notification preferences only. It is used when the
-	 * activity is showing a two-pane settings UI.
-	 */
-	public static class AboutPreferenceFragment extends PreferenceFragment {
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			addPreferencesFromResource(R.xml.pref_about);
-
-			String str = null;
-			try {
-				Preference version = findPreference("build_version");
-				if (version != null) {
-					version.setSummary(getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName);
-				}
-			} catch (PackageManager.NameNotFoundException e) {
-				e.printStackTrace();
-			}
-			Preference localPreference5 = findPreference("version");
-			if (localPreference5 != null)
-				localPreference5.setSummary(str);
-
-			findPreference("open_source_license_info").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-				@Override
-				public boolean onPreferenceClick(Preference preference) {
-					getActivity().startActivity(new Intent(getActivity(), OpenSourceLicenseInfoActivity.class));
-					return true;
-				}
-			});
-
-			findPreference("company").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-				@Override
-				public boolean onPreferenceClick(Preference preference) {
-					Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://asrcfederal.com/mse"));
-					startActivity(browserIntent);
-					return true;
-				}
-			});
+			bindPreferenceSummaryToValue(findPreference(getString(R.string.key_units_temperature)));
+			bindPreferenceSummaryToValue(findPreference(getString(R.string.key_units_pressure)));
+			bindPreferenceSummaryToValue(findPreference(getString(R.string.key_units_wind_speed)));
+			bindPreferenceSummaryToValue(findPreference(getString(R.string.key_units_wind_direction)));
 		}
 	}
 
@@ -209,7 +138,52 @@ public class SettingsActivity extends PreferenceActivity {
 			// to their values. When their values change, their summaries are
 			// updated to reflect the new value, per the Android Design
 			// guidelines.
-			bindPreferenceSummaryToValue(findPreference("sync_frequency"));
+			bindPreferenceSummaryToValue(findPreference(getString(R.string.key_sync_frequency)));
+		}
+	}
+
+	/**
+	 * This fragment shows about preferences only.
+	 */
+	public static class AboutPreferenceFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.pref_about);
+
+			Preference version = findPreference(getString(R.string.key_about_version));
+			if (version != null) {
+				try {
+					PackageInfo packageInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+					version.setSummary(packageInfo.versionName + " (" + packageInfo.versionCode + ")");
+				} catch (PackageManager.NameNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+
+			Preference licenseInfo = findPreference(getString(R.string.key_about_open_source_licenses));
+			if (licenseInfo != null) {
+				licenseInfo.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						getActivity().startActivity(new Intent(getActivity(), OpenSourceLicenseInfoActivity.class));
+						return true;
+					}
+				});
+			}
+
+			Preference company = findPreference(getString(R.string.key_about_company_name));
+			if (company != null) {
+				company.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://asrcfederal.com/mse"));
+						startActivity(browserIntent);
+						return true;
+					}
+				});
+			}
+
 		}
 	}
 }
