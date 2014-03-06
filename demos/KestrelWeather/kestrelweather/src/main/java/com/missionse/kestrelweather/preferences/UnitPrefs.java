2e7641cd84ff7030ce07f88c36d761c81aef1c3a
Kyle <kyle.h.lehman@gmail.com>
2014-03-06 12:46:00 -0500

    Implemented temperature preferences in KestrelWeather

--- demos/KestrelWeather/kestrelweather/src/main/java/com/missionse/kestrelweather/preferences/UnitPrefs.java
+++ demos/KestrelWeather/kestrelweather/src/main/java/com/missionse/kestrelweather/preferences/UnitPrefs.java
@@ -1,6 +1,11 @@
 package com.missionse.kestrelweather.preferences;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.missionse.kestrelweather.R;
+import com.missionse.kestrelweather.util.UnitConverter;
 
 /**
  * Utility class for getting weather measurements in the preferred units.
@@ -11,36 +16,56 @@ public final class UnitPrefs {
 	}
 
 	/**
-	 * Get the measure in the preferred unit for temperature.
-	 * @param context the current context
-	 * @param temperature the measured temperature
+	 * Get the measurement in the preferred unit for temperature.
+	 *
+	 * @param context     the current context
+	 * @param temperature the measured temperature in celsius
 	 * @return temperature in the preferred units
 	 */
 	public static float getPreferredTemperature(Context context, float temperature) {
-		return temperature;
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		String key = context.getString(R.string.key_units_temperature);
+		String value = prefs.getString(key, context.getString(R.string.celsius));
+		if (value.equals(context.getString(R.string.fahrenheit))) {
+			return UnitConverter.celsiusToFahrenheit(temperature);
+		} else {
+			return temperature;
+		}
 	}
 
 	/**
 	 * Gets the preferred unit for temperature.
+	 *
 	 * @param context the current context
 	 * @return the preferred unit for temperature
 	 */
 	public static String getPreferrredTemperatureUnit(Context context) {
-		return "Celsius (°C)";
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		String key = context.getString(R.string.key_units_temperature);
+		return prefs.getString(key, context.getString(R.string.celsius));
 	}
 
 	/**
 	 * Gets the preferred abbreviated unit for temperature.
+	 *
 	 * @param context the current context
 	 * @return the preferred unit for temperature
 	 */
 	public static String getPreferredTemperatureUnitAbbr(Context context) {
-		return "°C";
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		String key = context.getString(R.string.key_units_temperature);
+		String value = prefs.getString(key, context.getString(R.string.celsius));
+		if (value.equals(context.getString(R.string.fahrenheit))) {
+			return context.getString(R.string.fahrenheitAbbr);
+		} else {
+			return context.getString(R.string.celsiusAbbr);
+		}
 	}
 
 	/**
 	 * Get the measure in the preferred unit for pressure.
-	 * @param context the current context
+	 *
+	 * @param context  the current context
 	 * @param pressure the measured pressure
 	 * @return pressure in the preferred units
 	 */
@@ -50,6 +75,7 @@ public final class UnitPrefs {
 
 	/**
 	 * Gets the preferred unit for pressure.
+	 *
 	 * @param context the current context
 	 * @return the preferred unit for pressure
 	 */
@@ -59,6 +85,7 @@ public final class UnitPrefs {
 
 	/**
 	 * Gets the preferred abbreviated unit for pressure.
+	 *
 	 * @param context the current context
 	 * @return the preferred unit for pressure
 	 */
@@ -68,7 +95,8 @@ public final class UnitPrefs {
 
 	/**
 	 * Get the measure in the preferred unit for wind speed.
-	 * @param context the current context
+	 *
+	 * @param context   the current context
 	 * @param windSpeed the measured wind speed
 	 * @return wind speed in the preferred units
 	 */
@@ -78,6 +106,7 @@ public final class UnitPrefs {
 
 	/**
 	 * Gets the preferred unit for wind speed.
+	 *
 	 * @param context the current context
 	 * @return the preferred unit for wind speed
 	 */
@@ -87,6 +116,7 @@ public final class UnitPrefs {
 
 	/**
 	 * Gets the preferred abbreviated unit for wind speed.
+	 *
 	 * @param context the current context
 	 * @return the preferred unit for wind speed
 	 */
@@ -96,16 +126,18 @@ public final class UnitPrefs {
 
 	/**
 	 * Get the measure in the preferred unit for wind direction.
-	 * @param context the current context
+	 *
+	 * @param context       the current context
 	 * @param windDirection the measured wind direction
 	 * @return wind direction in the preferred units
 	 */
-	public static float getPreferredWindDirection(Context context, int windDirection) {
+	public static float getPreferredWindDirection(Context context, float windDirection) {
 		return windDirection;
 	}
 
 	/**
 	 * Gets the preferred unit for wind direction.
+	 *
 	 * @param context the current context
 	 * @return the preferred unit for wind direction
 	 */
@@ -115,6 +147,7 @@ public final class UnitPrefs {
 
 	/**
 	 * Gets the preferred abbreviated unit for wind direction.
+	 *
 	 * @param context the current context
 	 * @return the preferred unit for wind direction
 	 */
