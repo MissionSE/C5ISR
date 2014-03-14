37276aa08c7c2d0032805d2b8b2461991da1fb80
jsantangelo <jamesfsantangelo@gmail.com>
2014-03-14 12:30:15 -0400

    Potentially fixed issue wherein the Kestrel Weather server app would sometimes return a 0 instead of the actual latest event id, when doing /event/since calculations.

--- demos/KestrelWeather/web/lib/handler/getEventsSince.js
+++ demos/KestrelWeather/web/lib/handler/getEventsSince.js
@@ -11,31 +11,33 @@ module.exports = function(db) {
 		var latestEventId = 0;
 		db.Event.nextCount(function(err, count) {
 			latestEventId = count - 1;
-		});
-		db.Event.find( { _id: { $gt: lastClientEventId } }).exec(function(err, events) {
-			if (events) {
-				for (var index = 0; index < events.length; index++) {
-					if (events[index].eventType === 'create' ||
-						events[index].eventType === 'modify') {
-						fetchableReportIds.push(events[index].reportId);
-					} else if (events[index].eventType === 'delete') {
-						fetchableReportIds = _.without(fetchableReportIds, events[index].reportId);
-						removableReportIds.push(events[index].reportId);
-					}
 
-					if (events[index]._id > latestEventId) {
-						latestEventId = events[index]._id;
+			db.Event.find( { _id: { $gt: lastClientEventId } }).exec(function(err, events) {
+				if (events) {
+					for (var index = 0; index < events.length; index++) {
+						if (events[index].eventType === 'create' ||
+							events[index].eventType === 'modify') {
+							fetchableReportIds.push(events[index].reportId);
+						} else if (events[index].eventType === 'delete') {
+							fetchableReportIds = _.without(fetchableReportIds, events[index].reportId);
+							removableReportIds.push(events[index].reportId);
+						}
+
+						if (events[index]._id > latestEventId) {
+							latestEventId = events[index]._id;
+						}
 					}
+					fetchableReportIds = _.uniq(fetchableReportIds);
 				}
-				fetchableReportIds = _.uniq(fetchableReportIds);
-			}
-
-			res.writeHead(200, {'content-type': 'text/plain'});
-			res.end(JSON.stringify({
-				latestEvent: latestEventId,
-				toFetch: fetchableReportIds,
-				toRemove: removableReportIds
-			}));
+				
+				res.writeHead(200, {'content-type': 'text/plain'});
+				res.end(JSON.stringify({
+					latestEvent: latestEventId,
+					toFetch: fetchableReportIds,
+					toRemove: removableReportIds
+				}));
+			});
 		});
+
 	}
 };
